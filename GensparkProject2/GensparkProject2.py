11958d531b69d26a36f1cc667e92efe85ae3a39e
Muab-Dib <jpsteph1@asu.edu>
2025-02-02 16:15:32 -0700

    Commented it

--- GensparkProject2/GensparkProject2.py
+++ GensparkProject2/GensparkProject2.py
@@ -1,15 +1,15 @@
 import random
 
-num_to_guess = random.randint(1, 100)
+num_to_guess = random.randint(1, 101) # random number between 1 and 100
 
 i = 1
 while i < 11:
-    attemptVal = int(input("Guess the number (between 1 and 100): "))
-    if attemptVal > num_to_guess:
+    attemptVal = int(input("Guess the number (between 1 and 100): ")) # prompt user input
+    if attemptVal > num_to_guess: # if the number is less than the random number, then we tell them its too high
         print("Too high! Try again.")
-    elif attemptVal < num_to_guess:
+    elif attemptVal < num_to_guess: # if their guess was too low, we tell them that
         print("Too low! Try again.")
-    else:
+    else: # if they get it right ,we tell em the number of guesses it took
         print(f"Congratulations! You guessed it in {i} attempts!")
         break
     i += 1
